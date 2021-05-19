2dbeffe1ebd1f4d3d7b0866f73941d01aa52e481
Adam Harper <adamharper@Adams-MacBook-Pro-2.local>
2021-05-19 10:48:25 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var randomise = document.getElementById("random");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
+
+
+const randomBG = () => {
+	const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+	return "#" + randomColor;
+}
+
+function randomGradient(){
+	color1.value = randomBG();
+	color2.value = randomBG();
+
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+random.addEventListener("click", randomGradient)
